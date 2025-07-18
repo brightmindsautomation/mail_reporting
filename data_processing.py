62ed3d85147ae1efc2dfca7686fb932a0fc66c84
SarmiN92 <sarmid97@gmail.com>
2025-07-19 03:38:15 +0530

    Generating other report for report_config = 3. Code changes

--- data_processing.py
+++ data_processing.py
@@ -1,26 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 16 15:51:37 2025
+
+
+"""
+
 # data_processing.py
 import pandas as pd
-import os
+pd.options.mode.chained_assignment = None  # default='warn'
+#import os
+#from boolean_parser import parse
 from email_sender import send_email
 from config import RECIPIENT_EMAILS, FILE_PATH
 
-def filter_excel_rows(start_value, end_value):
-    df = pd.read_excel(FILE_PATH, header=0, parse_dates=[0])
-    df.insert(0, "Datetime", df.pop(df.columns[0]))
+from input_handler import get_user_input
 
-    filtered_df = df[(df["Datetime"] >= start_value) & (df["Datetime"] <= end_value)]
-    selected_df = filtered_df[(filtered_df["6001.P"] > 6000) & (filtered_df["6002.P"] < 2000)]
 
-    total_points = len(filtered_df)
-    up_time_row = {col: 0 for col in selected_df.columns}
-    up_time_row["Datetime"] = "Up Time"
+def filter_excel_rows(start_value, end_value, item_name_list, report_item_name_list, threshold_tags, threshold_logic, grouped_columns, normal_state, excluded, config, percentage_auto, percentage_manual, report_config, emails):
+    #threshold_percentage = 80
+    df = pd.read_excel(FILE_PATH, header=0, parse_dates=[0]) #read excel file
+    df.insert(0, "Datetime", df.pop(df.columns[0])) #insert datetime
+    
+    validation_tags = config.loc[config["Item_Type"] == "Validation"]["Item_Name"].values #the tags involved in the logic
+    val_dict = {}
+    val_dict_rename = {}
+    for i in range(len(validation_tags)):
+        val_dict.update({validation_tags[i]: "val" + str(i)})
+        val_dict_rename.update({"val" + str(i): validation_tags[i]}) #create dictionaries to rename columns since .query() func does not accept col names with periods
+        threshold_logic = threshold_logic.replace(validation_tags[i], "val" + str(i)) #replace column names in logic
+    threshold_logic = threshold_logic.strip()
+    threshold_logic = threshold_logic.replace("AND", "&")
+    df.rename(columns = val_dict, inplace = True)
+    
 
-    excluded_columns = {"6001.P", "6002.P", "4022.M", "4040.M", "4045.M"}
+    filtered_df = df[(df["Datetime"] >= start_value) & (df["Datetime"] <= end_value)] #select based on dates
+    selected_df = filtered_df.query(threshold_logic) #filter df according to logic supplied in Validation items
+    selected_df.rename(columns = val_dict_rename, inplace=True) #rename columns
+    filtered_df.rename(columns = val_dict_rename, inplace=True)
+    df.rename(columns = val_dict_rename, inplace=True)
+
+    #total_points = len(filtered_df)
+    up_time_row = {col: 0 for col in selected_df.columns} #initialize UpTime row
+    up_time_row["Datetime"] = "Up Time" 
+
+    validation_cols = config.loc[config["Item_Type"] != "Report"]["Item_Name"].values.tolist() #columns to remove based on Item Type
+
+    excluded_columns = set(df.columns) - set(item_name_list)
+    excluded_columns = excluded_columns.union(set(excluded["Item_Name"].values.tolist())).union(set(validation_cols))
     percentage_row = {col: 0 for col in selected_df.columns}
     percentage_row["Datetime"] = "Percentage"
-
+    
     percentage_values = []
-    grouped_columns = {"Group1": ["4009.M", "4010.M"], "Group2": ["4012.M", "4013.M", "4014.M"]}
+    grouped_cols = {}
+    for i in range(len(grouped_columns)):
+        if len(grouped_columns.iloc[i])>1:
+            grouped_cols.update({grouped_columns.index[i]: grouped_columns.iloc[i]})
+    grouped_columns = grouped_cols
     min_percentage_per_group = {}
 
     for col in selected_df.columns[1:]:
@@ -31,11 +66,9 @@ def filter_excel_rows(start_value, end_value):
             total_count = filtered_df[col].count()
             up_time = round((valid_count / total_count) * 100, 2) if total_count > 0 else 0
             up_time_row[col] = up_time
-
-            if col == "4070.M":
-                percent_count = (selected_df[col] == "MA").sum()
-            else:
-                percent_count = (selected_df[col] == "AU").sum()
+            
+            normal = normal_state.loc[normal_state["Item_Name"]==col]["Normal State"].values[0]
+            percent_count = (selected_df[col]== normal).sum()
 
             percentage = round((percent_count * 100) / valid_count, 2) if valid_count > 0 else 0
             percentage_row[col] = percentage
@@ -48,18 +81,66 @@ def filter_excel_rows(start_value, end_value):
 
     table_data = []
     for col, percentage in percentage_row.items():
-        if col != "Datetime" and percentage < 80:
-            table_data.append([col, "Desc " + col, percentage])
+        if col not in excluded_columns:
+            report_col = report_item_name_list.loc[report_item_name_list["Item_Name"]==col]["Report_Item_Name"].values            
+            description = config.loc[config["Item_Name"]==col]["Description"].values
+            if len(report_col)>0:
+                report_col = report_col[0]
+            if len(description)>0:
+                description = description[0]
+            if col != "Datetime":    
+                if report_config == 3:
+                    try:
+                        filtered_series = report_item_name_list.loc[report_item_name_list["Item_Name"] == col, "Report_Item_Name"].fillna("")
+                        if filtered_series.size > 0 and filtered_series.iloc[0].upper().endswith(".SP"):                            
+                            # table_data.append([filtered_series.iloc[0], "0"])
+                            sp_cols = [col for col in selected_df.columns if col.upper().endswith(".SP")]
+                            selected_df = selected_df[sp_cols]
+                    except:
+                        print('3. error in appending table data3.')                    
+                else:                                                
+                    if report_config == 1 and percentage >= percentage_auto:
+                        try:
+                            table_data.append([report_col, description, percentage])
+                        except:
+                            print('1. error in appending table data.')
+                    if report_config == 2 and percentage >= percentage_manual:
+                        try:
+                            table_data.append([report_col, description, percentage])
+                        except:
+                            print('2. error in appending table data.')                
 
     avg_percentage = round(sum(percentage_values) / len(percentage_values), 2) if percentage_values else 0
-
-    selected_df = pd.concat([selected_df, pd.DataFrame([up_time_row]), pd.DataFrame([percentage_row])], ignore_index=True)
+    if report_config == 3:
+        SPchanges = 0
+        for col in selected_df.columns:
+            SPchanges = (selected_df[col] != selected_df[col].shift(1)).sum() - 1
+            table_data.append([col, SPchanges])
+        changes_df = pd.DataFrame([SPchanges]).reindex(columns=selected_df.columns)      
+        selected_df = pd.concat([selected_df, pd.DataFrame([SPchanges])], ignore_index=True) 
+    else:
+        selected_df = pd.concat([selected_df, pd.DataFrame([up_time_row]), pd.DataFrame([percentage_row])], ignore_index=True)
+    for oldCol, newCol in zip(report_item_name_list["Item_Name"], report_item_name_list["Report_Item_Name"]):
+        if oldCol in selected_df.columns:
+            selected_df.rename(columns = {oldCol: newCol}, inplace=True)
+    
     report_file = "Generated_Report.xlsx"
-    selected_df.to_excel(report_file, index=False)
+    report_df = selected_df if report_config == 3 else selected_df.tail(2)
+    report_df_t = report_df.transpose()
+    report_df_t.to_excel(report_file, index=True, header=False)
 
-    table_df = pd.DataFrame(table_data, columns=["Item Name", "Description", "Percentage in Normal State"])
-
-    for email in RECIPIENT_EMAILS:
-        send_email(table_df, avg_percentage, email, report_file)
+    if report_config == 1:
+        table_df = pd.DataFrame(table_data, columns=["Item Name", "Description", "Percentage in Auto State"])
+    elif report_config == 2:
+        table_df = pd.DataFrame(table_data, columns=["Item Name", "Description", "Percentage in Manual State"])
+    elif report_config == 3:
+        table_df = pd.DataFrame(table_data, columns=["Item Name", "Number of Changes occured"])
+        
+    if type(emails) == list:
+        emails = emails[0]
+    send_email(table_df, avg_percentage, emails, report_file)
     
     return selected_df
+
+#start_value, end_value, item_name_list, report_item_name_list, threshold_tags, threshold_logic, grouped_columns, normal_state, excluded, config, percentage_auto, percentage_manual, report_config, emails = get_user_input()
+#filter_excel_rows(start_value, end_value, item_name_list,  report_item_name_list,threshold_tags, threshold_logic, grouped_columns, normal_state, excluded, config, percentage_auto, percentage_manual, report_config, emails)
--- input_handler.py
+++ input_handler.py
@@ -3,10 +3,93 @@ import pandas as pd
 import logging
 
 def get_user_input():
+    debugging = 0
     try:
-        start_value = pd.to_datetime(input("Enter the start date (MM-DD-YYYY): "))
-        end_value = pd.to_datetime(input("Enter the end date (MM-DD-YYYY): "))
-        return start_value, end_value
-    except Exception as e:
-        logging.error("Invalid date input. Please enter dates in MM-DD-YYYY format.", exc_info=True)
-        raise
+        with open("input.txt") as f:
+          #print("open.0")
+          for x in f:
+            #x = x.lower()
+            if "Date_From =" in x:
+                start_value = x.split("=")[1].split(" ")[1:3]
+                start_value = start_value[0] + ' ' + start_value[1]
+                start_value = pd.to_datetime(start_value)
+                if debugging == 1:
+                    print("date_from")
+            
+            if "Date_To =" in x:
+                #print('date to')
+                end_value = x.split("=")[1].split(" ")[1:3]
+                end_value = end_value[0] + ' ' +  end_value[1]
+                end_value = pd.to_datetime(end_value)
+                if debugging == 1:
+                    print("date_to")
+                
+            if "Validation items" in x:
+                x.replace("AND", "&")
+                x.replace("OR", "|")
+                x.replace("\n", "")
+                threshold_logic = x.split("items")[1]
+                if debugging == 1:
+                    print("validation_items")
+            if "Threshold_percentage_auto" in x:
+                percentage_auto = x.split("=")[1].strip()
+                percentage_auto = percentage_auto.split("%")[0]
+                percentage_auto = percentage_auto.strip()
+                try:
+                    percentage_auto = float(percentage_auto)
+                except:
+                    print("Unknown Threshold_percentage_auto. Please update input.txt")
+                if debugging == 1:
+                    print("threshold_percentage_auto")
+            if "Threshold_percentage_manual" in x:
+                percentage_manual = x.split("=")[1].strip()
+                percentage_manual = percentage_manual.split("%")[0]
+                percentage_manual = percentage_manual.strip()
+                try:
+                    percentage_manual = float(percentage_manual)
+                except:
+                    print("Unknown Threshold_percentage_auto. Please update input.txt")
+                if debugging == 1:
+                    print("threshold_percentage_manual")
+            if "Report_Configuration" in x:
+                report_config = x.split("=")[1].strip()
+                report_config = report_config.split(" ")[0]
+                report_config = report_config.strip()
+                report_config = int(report_config)
+                if report_config not in [1,2,3]:
+                    print("unknown report configuration. Please update input.txt")
+                if debugging == 1:
+                    print("report_configuration")
+            if "email addresses" in x:
+                emails = x.split("=")[1].strip().split(" ")[0]
+                emails = emails.split(";")
+                if debugging == 1:
+                    print("emails")
+                    
+
+        f.close()
+        
+    except:
+        print('Error. input file not found')
+    try:
+            #print('hi!')
+            config = pd.read_excel("Config_Data_new.xlsx")
+            oldCols = config.columns
+            cols = [value.rstrip() for value in oldCols]
+            colDict = {}
+            for i in range(len(cols)):
+                colDict.update({oldCols[i]: cols[i]})
+            config.rename(columns = colDict, inplace=True)
+            item_name_list = config.loc[config["Item_Type"] == "Report"]["Item_Name"].to_list()
+            report_item_name_list = config[["Item_Name", "Report_Item_Name"]]
+            grouped_columns = config.groupby("Grouping")["Item_Name"].apply(list)
+            threshold_tags = ['6001.P', '6002.P']
+            normal_state = config[["Item_Name", "Normal State"]]
+            excluded = config.loc[config["Exclude"] == "Y"]
+            #print('end')
+            return start_value, end_value, item_name_list, report_item_name_list, threshold_tags, threshold_logic, grouped_columns, normal_state, excluded, config, percentage_auto, percentage_manual, report_config, emails
+    except:
+            print('Error. Config_Data file not found.')
+            
+
+get_user_input()
